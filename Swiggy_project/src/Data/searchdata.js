e45b9acbbeba6bbdb836fddc571eede7b0fc710a
Md Khalid Alam <sonualam96314@gmail.com>
2024-11-12 21:26:46 +0530

    add search section

--- Swiggy_project/src/Data/searchdata.js
+++ Swiggy_project/src/Data/searchdata.js
@@ -1,23 +1,38 @@
 export const PopularFood = [
     {
-        imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/3df4fca020027e89b89c733cdffc4966",
+        id: "1",
+        imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/b4ff78ecc5b8b66f732dd06228916d65",
     },
     {
+        id: "2",
         imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/3df4fca020027e89b89c733cdffc4966"
     },
     {
+        id: "3",
+        imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/e76b511935016406e6ebc11dd7593387"
+    },
+    {
+        id: "4",
         imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/87664acb0f9dd95d10a549bb8190ab27"
     },
     {
-        imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/87664acb0f9dd95d10a549bb8190ab27"
-    },
-    {
+        id: "5",
         imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/31f03222ea978aef3b10d386729eb076"
     },
     {
+        id: "6",
         imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/36184033ebef97d27a85fa3af5c1d403"
     },
     {
-        imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/36184033ebef97d27a85fa3af5c1d403"
+        id: "7",
+        imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/5dd234f7decdac4b4f71a2ff1408e10f"
+    },
+    {
+        id:"8" ,
+        imgUrl : "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/c170aa4262ec0d191642f42a3a03b4ce"
+    },
+    {
+        id:"9",
+        imgUrl : "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/d0884e09ef431ee610e54a0bb2dfecd5"
     }
 ]
--- Swiggy_project/src/components/Search/SearchPage.jsx
+++ Swiggy_project/src/components/Search/SearchPage.jsx
@@ -1,41 +1,48 @@
 import React from 'react'
 import { CiSearch } from "react-icons/ci";
 import { PopularFood } from '../../Data/searchdata';
+import "./SearchPage.css"
 
 const SearchPage = () => {
     return (
-        <div>
-            <div>
+        <div className='search_wrapper'>
+            <div className='search_container'>
 
                 <div>
-                    <form>
-                        <div>
+                    <form className='input_search_container'>
+                        <div className='input_search'>
                             <input type='text' placeholder='Search for restaurants and food' />
                         </div>
 
-                        <div>
+                        <div className='input_icon'>
                             <CiSearch />
                         </div>
                     </form>
+
                 </div>
 
                 <div>
-                    <div>
-                        <p>Recent Searches</p>
-                        <div>
-                            <p><span><CiSearch /></span> Kanha Ji Rasoi</p>
+
+                    <div className='recent_container'>
+                        <p className='p'>Recent Searches</p>
+                        <div className='recent_container_icon'>
+                            <CiSearch />
+                            <p>Kanha Ji Rasoi</p>
                         </div>
                     </div>
-                    <div>
+
+                    <div className='popular_image'>
                         {
-                            PopularFood.map( (url) => (
-                                <div>
-                                    
+                            PopularFood.map((url) => (
+                                <div key={url.id}>
+                                    <img src={url.imgUrl} />
                                 </div>
                             ))
                         }
                     </div>
+
                 </div>
+
             </div>
         </div>
     )
