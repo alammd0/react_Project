56aff0aae7127c3af0abb36f21bd956f61a7a3a9
Md Khalid Alam <sonualam96314@gmail.com>
2024-11-20 09:59:47 +0530

    update the search page

--- Swiggy_project/src/components/Search/Details.jsx
+++ Swiggy_project/src/components/Search/Details.jsx
@@ -8,36 +8,41 @@ const Details = ({ category }) => {
 
 
     const [tabName, setTabName] = useState('Dish');
+    const [openClose, setOpenClose] = useState(true);
 
     const handleTabName = (tab) => {
         setTabName(tab);
+        setOpenClose(true);
+    }
+
+    const handleOpenClose = () => {
+        setOpenClose(false);
     }
 
     console.log(category)
 
     return (
-            <div className='tab_res'>
-                <div className='tab_name'>
-                    {
-                        searchTab.map((tab) => (
-                            <div className={tabName === tab.name ? 'tab-active' : 'tab-inactive'} key={tab.id} onClick={() => handleTabName(tab.name)}>
-                                <button>{tab.name}</button>
-                            </div>
-                        ))
-                    }
-                </div>
-
-                <div className='foods_card_relative_cont'>
-                    {
-                        tabName && (
-                            <div>
-                                <TabFilterPage category={category} tabName={tabName} />
-                            </div>
-                        )
-                    }
-
-                </div>
+        <div className='tab_res'>
+            <div className='tab_name'>
+                {
+                    searchTab.map((tab) => (
+                        <div className={tabName === tab.name ? 'tab-active' : 'tab-inactive'} key={tab.id} onClick={() => handleTabName(tab.name)}>
+                            <button>{tab.name}</button>
+                        </div>
+                    ))
+                }
             </div>
+
+            <div className='foods_card_relative_cont'>
+                {
+                    openClose && (
+                        <div>
+                            <TabFilterPage category={category} tabName={tabName} handleOpenClose={handleOpenClose}/>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
     )
 }
 
--- Swiggy_project/src/components/Search/TabFilterPage.jsx
+++ Swiggy_project/src/components/Search/TabFilterPage.jsx
@@ -2,19 +2,26 @@ import React, { useState } from 'react'
 import DetailsFood from './DetailsFood';
 import Restaurent from './Restaurent';
 
-const TabFilterPage = ({ tabName, category }) => {
+const TabFilterPage = ({ tabName, category, handleOpenClose }) => {
 
     console.log(tabName);
 
+
     return (
         <div className="footDetail_wrapper">
-            {
-                tabName === 'Dish' ? (
-                    <DetailsFood category={category} />
-                ) : (
-                    <Restaurent category={category} />
-                )
-            }
+            <div>
+                {
+                    tabName === 'Dish' ? (
+                        <DetailsFood category={category} />
+                    ) : (
+                        <Restaurent category={category} />
+                    )
+                }
+            </div>
+            <div className='CloseBtn'>
+                <button onClick={handleOpenClose}>Close</button>
+            </div>
+
         </div>
     )
 }
