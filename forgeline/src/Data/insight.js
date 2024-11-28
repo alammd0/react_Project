156676c1d4000c7d91d8cf5b50b8a1655ebd8279
Md Khalid Alam <sonualam96314@gmail.com>
2024-11-28 12:18:55 +0530

    some add

---
+++ forgeline/src/Data/insight.js
@@ -0,0 +1,26 @@
+export const insightData = [
+    {
+        id: "1",
+        title: "The IMTS Success Blueprint with Bonnie Gurney and Michelle Edmonson",
+        desc: "Join AMT's Bonnie Gurney and Michelle Edmonson as they explore the future of trade shows, sharing insights and strategies for navigating the evolving landscape of events like IMTS.",
+        url: "https://industrialstrengthmarketing.com/img/uploads/michelle_bonnie_thumbnail.png"
+    },
+    {
+        id: "2",
+        title: "The Future of Trade Shows: How IMTS Builds Innovation and Community",
+        desc: "Peter Eelman, CXO of AMT, discusses how IMTS has become a leading event in manufacturing technology, fostering innovation and community.",
+        url: "https://industrialstrengthmarketing.com/img/uploads/ind_peter_eelman_thumbnail.png"
+    },
+    {
+        id: "3",
+        title: "8 B2B Marketing Services That Drive Business Growth",
+        desc: "In this article, we’ll share 10 B2B marketing services that drive business growth for manufacturers and industrial companies.",
+        url: "https://industrialstrengthmarketing.com/img/uploads/thumbnail_308x215.jpg"
+    },
+    {
+        id: "4",
+        title: "7 Ways to Support Effective SEO for Manufacturers",
+        desc: "In this article, SEO for manufacturers, we’ll share best practices to help you make your website easier to find for industrial buyers.",
+        url: "https://industrialstrengthmarketing.com/img/uploads/cap-cs-thumbnail_0002_adobestock_225407331.jpg"
+    }
+]
